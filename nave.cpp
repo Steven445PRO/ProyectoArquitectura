f7166e60f4c1edaef8b879b34d10fc991897d6cd
Steven445PRO <167663198+Steven445PRO@users.noreply.github.com>
2024-05-27 13:54:56 -0600

    Add files via upload

---
+++ nave.cpp
@@ -0,0 +1,30 @@
+#include "nave.h"
+#include <ncurses.h>
+
+void nave::setup() {
+    m_x = 60;
+    m_y = 20;
+}
+
+/*Esta hay que modificarla dependiendo de como queramos dibujar la nave*/
+void nave::draw() {
+    mvaddch(m_y, m_x + 2, '*');
+    mvaddch(m_y + 1 , m_x + 1, '**');
+    mvaddch(m_y + 2 , m_x , '***');
+}
+
+void nave::setX(int x) { 
+    m_x = x;
+}
+
+void nave::setY(int y) {
+    m_y = y;
+}
+
+int nave::getX() {
+    return m_x;
+}
+
+int nave::getY() {
+    return m_y;
+}
